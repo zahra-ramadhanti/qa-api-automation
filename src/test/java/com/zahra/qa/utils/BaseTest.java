07c782cd8237150d692059518b2acc4e778f252b
Angeli <angeli@MacBook-Air-Angeli.local>
2025-09-14 20:57:55 +0700

    create a base utils setup to define the api

---
+++ src/test/java/com/zahra/qa/utils/BaseTest.java
@@ -0,0 +1,18 @@
+
+package com.zahra.qa.utils;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BaseTest {
+
+	@BeforeEach
+	public void setUp() {
+		// Setup code before each test
+	}
+
+	@AfterEach
+	public void tearDown() {
+		// Cleanup code after each test
+	}
+}
